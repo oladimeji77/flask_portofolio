057fb3c49b4b88ce964b795cea89953e53025ab2
Oladimeji Oladepo <oladimeji.oladepo@gmail.com>
2023-06-30 18:27:38 +0100

    remove template files not needed

--- env/templates/test.py
+++ env/templates/test.py
@@ -1,20 +1,15 @@
-from datetime import date
+["Linux Foundation Certified System Administrator (LFCS)"","ITIL 4 Foundation IT Service Management","Microsoft Certified: Azure Cloud Administrator.","AWS Certified Cloud Practitioner.",
+"Oracle Cloud Infrastructure Foundations 2021 Associate.","CompTIA ITF+ certified.", "ISO/IEC 27001 Information Security Associate™.","Aviatrix Certified Engineer: Aviatrix Multi-Cloud Networking Associate."]
+•	Microsoft Certified Trainer (MCT).
+•	CertiProf: Cyber Security Foundation Professional Certificate CSFP. 
+•	Management and Strategy Institute: Lean Six Sigma White Belt Certified. 
+•	CertiProf: Remote work and Virtual Collaboration Profession Certificate.
+•	Cisco: Introduction to Cyber Security.
+•	Management and Strategy Institute: Project Management Essentials Certified. 
+•	Fortinet Certified Network Security Associate NSE 2.
+•	Micro Focus: Certified Mobile Center Professional.
+•	CertiProf: SCRUM Foundation Professional Certificate. 
+•	Microsoft Certified: Azure Cloud Fundamentals.
+•	Micro Focus: Arcsight Enterprise Security Manager ESM 7.3 Administrator /Analyst.
 
-today = date.today()
 
-# dd/mm/YY
-#d1 = today.strftime("%d/%m/%Y")
-d1 = today.strftime("%Y")
-print("d1 =", d1)
-
-# Textual month, day and year	
-d2 = today.strftime("%B %d, %Y")
-print("d2 =", d2)
-
-# mm/dd/y
-d3 = today.strftime("%m/%d/%y")
-print("d3 =", d3)
-
-# Month abbreviation, day and year	
-d4 = today.strftime("%b-%d-%Y")
-print("d4 =", d4)
